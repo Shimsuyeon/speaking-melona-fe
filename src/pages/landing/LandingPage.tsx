8ab8416253319ebb6fe11664661bf02a2f3036b2
Shim su yeon <84979352+Shimsuyeon@users.noreply.github.com>
2024-05-05 13:23:55 +0900

    pc view width edit

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import BananaMilk from "src/assets/bananaMilk.webp";
+import CreamBread from "src/assets/creamBread.webp";
 import Chatting from "src/components/chatting/Chatting";
 import WindowContainer from "src/components/windowContainer/WindowContainer";
 import { colorSet } from "src/constants";
@@ -6,39 +8,37 @@ import Paths from "src/types/paths";
 
 const LandingPage = () => {
   return (
-    <div className="flex flex-col px-4 h-screen justify-center">
-      <p className={"text-[55px] text-center font-hanna leading-none"}>
-        얘랑쟤랑? <br /> 대화궁합!
-      </p>
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col px-4 h-screen justify-center max-w-96">
+        <p className={"text-[55px] text-center font-hanna leading-none"}>
+          얘랑쟤랑? <br /> 대화궁합!
+        </p>
 
-      <div className={"h-[7%]"} />
+        <div className={"h-[7%]"} />
 
-      <WindowContainer>
-        <div className={"flex flex-col p-6 gap-4"}>
-          <Chatting
-            profileImage={
-              "https://avatars.githubusercontent.com/u/44080404?v=4"
-            }
-            text={"여러 상품을 스캔해서 대화를 생성해보세요!"}
-            direction={"right"}
-          />
-          <Chatting
-            profileImage={
-              "https://avatars.githubusercontent.com/u/44080404?v=4"
-            }
-            text={"나만의 대화 꿀조합은?"}
-            pointColor={colorSet.secondary}
-            direction={"left"}
-          />
+        <WindowContainer>
+          <div className={"flex flex-col p-6 gap-4"}>
+            <Chatting
+              profileImage={BananaMilk}
+              text={"여러 상품을 스캔해서 대화를 생성해보세요!"}
+              direction={"right"}
+            />
+            <Chatting
+              profileImage={CreamBread}
+              text={"나만의 대화 꿀조합은?"}
+              pointColor={colorSet.secondary}
+              direction={"left"}
+            />
+          </div>
+        </WindowContainer>
+
+        <div className={"h-[7%]"} />
+
+        <div className={"flex justify-center"}>
+          <Link to={Paths.Scan} className={"button w-[170px]"}>
+            <p className={"text-[25px]"}>시작하기</p>
+          </Link>
         </div>
-      </WindowContainer>
-
-      <div className={"h-[7%]"} />
-
-      <div className={"flex justify-center"}>
-        <Link to={Paths.Scan} className={"button w-[170px]"}>
-          <p className={"text-[25px]"}>시작하기</p>
-        </Link>
       </div>
     </div>
   );
--- src/pages/scan/ScanPage.tsx
+++ src/pages/scan/ScanPage.tsx
@@ -4,44 +4,48 @@ import Paths from "src/types/paths";
 
 const ScanPage = () => {
   return (
-    <div className="flex flex-col px-4 h-screen justify-center">
-      <p className={"text-[35px] text-center font-hanna"}>인식된 캐릭터</p>
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col px-4 h-screen justify-center max-w-96">
+        <p className={"text-[35px] text-center font-hanna"}>인식된 캐릭터</p>
 
-      <div className={"h-[3%]"} />
+        <div className={"h-[3%]"} />
 
-      <WindowContainer>
-        <div className={"grid grid-cols-3 gap-[30px_18px] px-4 py-[26px]"}>
-          {Array.from({ length: 6 }).map((_, index) => (
-            <button
-              key={index}
-              className={
-                "border border-dashed border-border aspect-[93/113] rounded-[10px] flex justify-center items-center"
-              }
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                viewBox="0 0 24 24"
-                fill="black"
+        <WindowContainer>
+          <div
+            className={"grid grid-cols-3 gap-[30px_18px] px-4 py-[26px] w-80"}
+          >
+            {Array.from({ length: 6 }).map((_, index) => (
+              <button
+                key={index}
+                className={
+                  "border border-dashed border-border aspect-[93/113] rounded-[10px] flex justify-center items-center"
+                }
               >
-                <rect y="11" width="24" height="2" />
-                <rect x="11" y="0" width="2" height="24" />
-              </svg>
-            </button>
-          ))}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  fill="black"
+                >
+                  <rect y="11" width="24" height="2" />
+                  <rect x="11" y="0" width="2" height="24" />
+                </svg>
+              </button>
+            ))}
+          </div>
+        </WindowContainer>
+
+        <div className={"h-[3%]"} />
+
+        <div className={"flex flex-col items-center gap-4"}>
+          <button className={"button w-[240px] bg-secondary"}>
+            <p className={"text-[25px]"}>추가 스캔</p>
+          </button>
+
+          <Link to={Paths.Result} className={"button w-[240px] bg-primary"}>
+            <p className={"text-[25px]"}>대화 생성</p>
+          </Link>
         </div>
-      </WindowContainer>
-
-      <div className={"h-[3%]"} />
-
-      <div className={"flex flex-col items-center gap-4"}>
-        <button className={"button w-[240px] bg-secondary"}>
-          <p className={"text-[25px]"}>추가 스캔</p>
-        </button>
-
-        <Link to={Paths.Result} className={"button w-[240px] bg-primary"}>
-          <p className={"text-[25px]"}>대화 생성</p>
-        </Link>
       </div>
     </div>
   );
